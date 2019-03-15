6a5d035bb9de5a1d7ae10e003dc09b74e787b1cc
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-15 13:54:06 +0300

    Change MainActivity layout

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SampleMLkitLabelImages
 Label images with Firebase ML Kit on Android.
+
+## Requirements
+* Android Studio 3.5 Canary 7
+* Kotlin 1.3.30-eap-11
+* Android Gradle Plugin 3.5.0-alpha07
+* Gradle wrapper 5.2.1
+* AAPT 2
+
+## References
+https://firebase.google.com/docs/ml-kit/label-images
--- app/src/main/java/com/gpetuhov/android/samplemlkitlabelimages/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplemlkitlabelimages/MainActivity.kt
@@ -3,6 +3,9 @@ package com.gpetuhov.android.samplemlkitlabelimages
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Image for labeling is taken from:
+// https://blog.usejournal.com/flutter-for-android-developers-how-to-design-activity-ui-in-flutter-4bf7b0de1e48
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
